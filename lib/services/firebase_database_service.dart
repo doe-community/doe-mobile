75987595bf9b03dafaef0fe2c50c664f1f55b4b0
Simeão David Lamine <simeao.david.lamine@ibm.com>
2020-03-12 23:18:07 -0300

    Add firestore cloud dependency and simple base implementation

---
+++ lib/services/firebase_database_service.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:doe/models/base_entity.dart';
+
+abstract class FirebaseDatabaseService {
+  Future<void> save(collection, BaseEntity data);
+}
+
+class _FireBaseDatabaseService implements FirebaseDatabaseService{
+
+  Firestore _firestore = Firestore.instance;
+
+  @override
+  Future<void> save(collection, BaseEntity data) async{///Data should be in json format
+    await _firestore
+              .collection(collection)
+              .document()
+              .setData(data.toJson());
+  }
+
+}
