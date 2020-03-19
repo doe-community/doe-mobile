d098a3183fe39a589a88e8a2fe34099261625f9b
Simeão David Lamine <simeaolm@gmail.com>
2020-03-19 14:58:14 -0300

    Create message service to handle notification

---
+++ lib/services/messaging_service.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:doe/services/firebase_database_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+
+class MessageService {
+  final Firestore _db = Firestore.instance;
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  final FirebaseDatabaseService databaseService;
+  final FirebaseUser user;
+
+  MessageService({this.user, this.databaseService,});
+
+
+
+  setupCloudMessaging() async{
+
+    var result = await databaseService.getUserDeviceToken(user.email);
+    if(result != null){//already has token associated
+     // return ;
+    }
+    
+    //configure fcm permission to work in IOS
+    if(Platform.isIOS){
+     // iosSubscription = 
+      _fcm.onIosSettingsRegistered.listen((data) => _saveDeviceToken());
+      _fcm.requestNotificationPermissions(
+        IosNotificationSettings(),
+      );
+    }else{
+      _saveDeviceToken();
+    }
+  }
+
+  setupNotificationHandler(BuildContext context){
+      _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("on message: $message");
+
+        final snackbar = SnackBar(
+          content: Text(message['notification']['title']),
+          action: SnackBarAction(
+            label: 'Go',
+            onPressed: () => null,
+            ), 
+        );
+        Scaffold.of(context).showSnackBar(snackbar);
+      },
+
+      onResume: (Map<String, dynamic> message) async {
+        print("on resume: $message");
+      },
+
+      onLaunch: (Map<String, dynamic> message) async {
+        print("on lauch: $message");
+      },
+    );
+  }
+
+  registerTopic(String topic){
+    _fcm.subscribeToTopic(topic);
+  }
+
+  unsubscribeTopic(String topic){
+    _fcm.unsubscribeFromTopic(topic);
+  }
+
+    //Get the token, save it to the database for current user
+  _saveDeviceToken() async {
+
+    String fcmToken = await _fcm.getToken();
+
+    if(fcmToken != null){
+      var tokenRef = _db.collection('profiles')
+      .document(user.email)
+      .collection('tokens')
+      .document(fcmToken);
+
+      tokenRef.setData({
+        'token': fcmToken,
+        'createdAt': FieldValue.serverTimestamp(),
+        'platforma': Platform.operatingSystem,
+      });
+
+    }
+
+  }
+  
+}
